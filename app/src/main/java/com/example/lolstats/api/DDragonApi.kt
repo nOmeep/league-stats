99a332887f04efce972e4513309e04af0d94068a
Sevastyan <sevapanov@yandex.ru>
2022-01-26 17:34:41 +0300

    api changes

--- app/src/main/java/com/example/lolstats/api/DDragonApi.kt
+++ app/src/main/java/com/example/lolstats/api/DDragonApi.kt
@@ -2,12 +2,13 @@ package com.example.lolstats.api
 
 import com.example.lolstats.api.items.GameChampions
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DDragonApi {
     companion object {
         const val BASE_URL = "http://ddragon.leagueoflegends.com/cdn/12.2.1/"
     }
 
-    @GET("/data/ru_RU/champion.json")
-    fun getAllChampions() : GameChampions
+    @GET("/data/{language}/champion.json")
+    fun getAllChampions(@Path("language") language : String) : GameChampions
 }
--- app/src/main/java/com/example/lolstats/api/items/GameChampions.kt
+++ app/src/main/java/com/example/lolstats/api/items/GameChampions.kt
@@ -1,14 +1,20 @@
 package com.example.lolstats.api.items
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameChampions(
-    val type : String,
+    val type: String,
     val version: String,
-    val data : List<Champion>
-) {
+    val data: Map<String, Champion>
+) : Parcelable {
+    @Parcelize
     data class Champion(
         val version: String,
-        val id : String,
-        val key : String,
-        val name : String
-    )
+        val id: String,
+        val key: String,
+        val name: String,
+        val title: String
+    ) : Parcelable
 }
