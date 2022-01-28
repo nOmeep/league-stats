a4091b9eb7397429178119e1a66c2de89dd2ddcd
Sevastyan <sevapanov@yandex.ru>
2022-01-28 23:14:55 +0300

    Search with edittext

--- app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
+++ app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
@@ -1,8 +1,11 @@
 package com.example.lolstats.ui.fragments.champions
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
@@ -69,12 +72,17 @@ class ChampionsFragment : Fragment(R.layout.fragment_champions) {
 
     private fun setSearchFunctionality() {
         binding.apply {
-            svFindChampions.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?) = false
+            etSearchChampion.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    (rvChampionList.adapter as ChampionsAdapter).filter(newText)
-                    return true
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    (rvChampionList.adapter as ChampionsAdapter).filter(p0)
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+
                 }
             })
         }
