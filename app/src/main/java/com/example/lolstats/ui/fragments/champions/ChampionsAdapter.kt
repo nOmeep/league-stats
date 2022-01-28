1ff92e9a6a7e1386c3a2847ca606ca2c3c37107d
Sevastyan <sevapanov@yandex.ru>
2022-01-28 22:57:58 +0300

    Search and filter champions

--- app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsAdapter.kt
+++ app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsAdapter.kt
@@ -8,9 +8,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.lolstats.api.items.Champion
 import com.example.lolstats.databinding.ItemChampionCircleBinding
+import java.util.*
 
 class ChampionsAdapter :
     ListAdapter<Champion, ChampionsAdapter.ChampionViewHolder>(ChampionComparator()) {
+
+    private var unfilteredList = listOf<Champion>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChampionViewHolder {
         val binding =
             ItemChampionCircleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -45,5 +49,24 @@ class ChampionsAdapter :
         override fun areContentsTheSame(oldItem: Champion, newItem: Champion) =
             oldItem == newItem
 
+    }
+
+    fun modifyList(list: List<Champion>) {
+        unfilteredList = list
+        submitList(list)
+    }
+
+    fun filter(query: CharSequence?) {
+        val list = mutableListOf<Champion>()
+
+        if(!query.isNullOrEmpty()) {
+            list.addAll(unfilteredList.filter {
+                it.name.lowercase(Locale.getDefault()).contains(query.toString().lowercase(Locale.getDefault())) ||
+                        it.name.lowercase(Locale.getDefault()).contains(query.toString().lowercase(Locale.getDefault())) })
+        } else {
+            list.addAll(unfilteredList)
+        }
+
+        submitList(list)
     }
 }
--- app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
+++ app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
@@ -2,8 +2,10 @@ package com.example.lolstats.ui.fragments.champions
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lolstats.R
 import com.example.lolstats.databinding.FragmentChampionsBinding
 import com.example.lolstats.ui.viewmodel.LeagueStatsViewModel
@@ -24,13 +26,57 @@ class ChampionsFragment : Fragment(R.layout.fragment_champions) {
         val championsAdapter = ChampionsAdapter()
 
         binding.apply {
-            rvChampionList.apply {
-                adapter = championsAdapter
-            }
+            rvChampionList.adapter = championsAdapter
+            setTopScrollAfterChange()
+            setSearchFunctionality()
+        }
 
-            viewModel.championList.observe(viewLifecycleOwner) { champions ->
-                championsAdapter.submitList(champions)
-            }
+        viewModel.championList.observe(viewLifecycleOwner) { champions ->
+            championsAdapter.modifyList(champions)
+        }
+    }
+
+    private fun setTopScrollAfterChange() {
+        binding.apply {
+            rvChampionList.adapter?.registerAdapterDataObserver(object :
+                RecyclerView.AdapterDataObserver() {
+                override fun onChanged() {
+                    rvChampionList.scrollToPosition(0)
+                }
+
+                override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+                    rvChampionList.scrollToPosition(0)
+                }
+
+                override fun onItemRangeMoved(fromPosition: Int, toPosition: Int, itemCount: Int) {
+                    rvChampionList.scrollToPosition(0)
+                }
+
+                override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                    rvChampionList.scrollToPosition(0)
+                }
+
+                override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
+                    rvChampionList.scrollToPosition(0)
+                }
+
+                override fun onItemRangeChanged(positionStart: Int, itemCount: Int, payload: Any?) {
+                    rvChampionList.scrollToPosition(0)
+                }
+            })
+        }
+    }
+
+    private fun setSearchFunctionality() {
+        binding.apply {
+            svFindChampions.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?) = false
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    (rvChampionList.adapter as ChampionsAdapter).filter(newText)
+                    return true
+                }
+            })
         }
     }
 
