2803fbc16115df281fafed5c8434005c9b974230
Sevastyan <sevapanov@yandex.ru>
2022-02-01 22:56:02 +0300

    little changes

--- app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
+++ app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsAnimation
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
