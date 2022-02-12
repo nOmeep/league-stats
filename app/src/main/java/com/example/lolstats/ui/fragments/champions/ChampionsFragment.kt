40b8d3995047648006d8449cde80b8a53daa7138
Sevastyan <sevapanov@yandex.ru>
2022-02-12 17:19:21 +0300

    loading progress

--- app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
+++ app/src/main/java/com/example/lolstats/ui/fragments/champions/ChampionsFragment.kt
@@ -2,6 +2,7 @@ package com.example.lolstats.ui.fragments.champions
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.lolstats.R
@@ -9,6 +10,7 @@ import com.example.lolstats.databinding.FragmentChampionsBinding
 import com.example.lolstats.ui.fragments.champions.listeners.ChampionSearcher
 import com.example.lolstats.ui.fragments.champions.listeners.OnChangeScroller
 import com.example.lolstats.ui.viewmodel.LeagueStatsViewModel
+import com.example.lolstats.util.cache.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,6 +33,9 @@ class ChampionsFragment : Fragment(R.layout.fragment_champions) {
 
         viewModel.getAllExistingChampions().observe(viewLifecycleOwner) { resource ->
             championsAdapter.modifyList(resource.data)
+
+            binding.pbLoading.isVisible = resource is Resource.Loading
+                    && resource.data.isNullOrEmpty()
         }
     }
 
